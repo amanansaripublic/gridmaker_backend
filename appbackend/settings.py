056366ff82599f01cdfc0d7bc15a55799c677e05
Aman Ansari <amanansari@Amans-Mac-mini.local>
2025-11-23 23:14:01 +0530

    added pythonsnywhere mysql db configurations

--- appbackend/settings.py
+++ appbackend/settings.py
@@ -90,10 +90,25 @@ WSGI_APPLICATION = 'appbackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'AnandBhalodi.mysql.pythonanywhere-services.com',
+        'NAME': 'AnandBhalodi$default',
+        'USER': 'AnandBhalodi',
+        'PASSWORD': 'Poorvik@8980732844',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
